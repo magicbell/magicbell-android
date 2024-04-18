7d7acc57354ef40a10b944d9bc195e9c121e5805
Christophe Clementi <christophe.clementi@winamp.com>
2024-04-18 14:02:48 +0200

    Fix HMAC calculation

--- sdk/src/main/java/com/magicbell/sdk/common/network/StringExt.kt
+++ sdk/src/main/java/com/magicbell/sdk/common/network/StringExt.kt
@@ -1,12 +1,23 @@
 package com.magicbell.sdk.common.network
 
 import android.util.Base64
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
-internal fun String.hmac(key: String): String {
-  val sha256Hmac = Mac.getInstance("HmacSHA256")
-  val secretKey = SecretKeySpec(key.toByteArray(), "HmacSHA256")
-  sha256Hmac.init(secretKey)
-  return Base64.encodeToString(this.toByteArray(), Base64.DEFAULT)
+fun String.hmac(key: String): String {
+    try {
+        val secretKeySpec = SecretKeySpec(key.toByteArray(charset("UTF-8")), "HmacSHA256")
+        val mac = Mac.getInstance("HmacSHA256")
+        mac.init(secretKeySpec)
+        val rawHmac = mac.doFinal(this.toByteArray(charset("UTF-8")))
+        val hmacBase64 = Base64.encodeToString(rawHmac, Base64.NO_WRAP)
+        return hmacBase64
+    } catch (e: NoSuchAlgorithmException) {
+        e.printStackTrace()
+    } catch (e: InvalidKeyException) {
+        e.printStackTrace()
+    }
+    return ""
 }
